a4b2672dd87211dc7b8efd9caf98729508c257cb
DevM7mdAli <DevM7mdAli@outlook.com>
2024-02-23 00:38:22 +0300

    adding Col and Sized Box

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     const String hello = "You Me I";
-    const String bye = "He she it Bye";
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar: AppBar(
         title: const Text(hello),
         backgroundColor: Colors.amber,
@@ -19,16 +19,37 @@ class MyApp extends StatelessWidget{
       ),
       
       body: SafeArea(
-        child:  Container(
-          color: Colors.blue,
-          height: 150,
-          width: 150,
-          margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 120),
-          padding: const EdgeInsets.all(10),
-          child: const Center(child: Text(bye)),
-        ),
+        child: Column(
+          //* mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //* verticalDirection: VerticalDirection.up, // reverse starting
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+            color: Colors.blue,
+            height: 150,
+            width: 120,
+            child: const Text("container 1"),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+            Container(
+            height: 100 ,
+            width: 140,
+            color: Colors.red,
+            child: const Text("Container 2"),
+          ),
+            Container(
+            height: 140 ,
+            width: 120,
+            color: Colors.indigo,
+            child: const Text("Container 3"),
+          ),
+            Container(//just for shape
+            width: double.infinity, //! making invesible full width
+          ),
+        ],),
       ),
-      
     );
   }
 }
