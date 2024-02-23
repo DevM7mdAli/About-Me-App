8c8ec13ec280fa9d326076520f34dc1012954200
DevM7mdAli <DevM7mdAli@outlook.com>
2024-02-23 16:13:10 +0300

    finish 99%

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp()
+      )
+    );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,17 +19,18 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text(hello),
         backgroundColor: Colors.amber,
-        toolbarHeight: 110,
+
       ),
-      body: SafeArea(
+      body: const SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const CircleAvatar(
+            CircleAvatar(
               radius: 50,
               backgroundImage: NetworkImage(
                   "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"),
             ),
-            const Text(
+            Text(
               "Camera Man",
               style: TextStyle(
                 fontSize: 36,
@@ -36,54 +39,59 @@ class MyApp extends StatelessWidget {
                 color: Colors.deepOrange,
               ),
             ),
-            const Text(
+            Text(
               "Like i Saud",
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
             ),
-            Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(3),
-              margin: const EdgeInsets.all(20),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.add_ic_call,
-                    size: 45,
-                    color: Colors.deepOrange,
-                  ),
-                  Text(
+
+            SizedBox(
+              width: 150,
+              height: 30,
+              child: Divider(
+                color: Colors.black54,
+                thickness: 2,
+              ),
+            ),
+
+            Card(
+              color: Colors.amber,
+              margin: EdgeInsets.all(20),
+              child: ListTile(
+                leading: Icon(Icons.add_ic_call,
+                              color: Colors.deepOrange,
+                              size: 45,
+                              ) ,
+                title: Text(
                     "0582539480",
                     style: TextStyle(
                       fontFamily: "Anta",
                       color: Colors.deepOrange,
                       fontSize: 30,
                     ),
-                  ),
-                ],
-              ),
+                  ), 
+              )
             ),
-          
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.all(3), margin: const EdgeInsets.all(20),
-            child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween ,
-              children: [
-              Icon(Icons.attach_email,
-                  color: Colors.deepOrange,
-                  size: 45,
-              ),
-              Text("CameraMan@gmailDead.com",
-                style: TextStyle(
+            Card(
+              color: Colors.amber,
+              margin: EdgeInsets.all(20),
+              child: ListTile(
+                leading: Icon(Icons.attach_email,
+                              color: Colors.deepOrange,
+                              size: 45,
+                ),
+
+                title: Text(
+                  "CameraMan@gmailDead.com",
+                  style: TextStyle(
                   fontFamily: "Anta",
-                  fontSize: 20,
+                  fontSize: 15,
                   color: Colors.deepOrange,
+                  ),
                 ),
               ),
-            ],),
-          ),
-        ],),
+            ),
+          ],
+        ),
       ),
     );
   }
